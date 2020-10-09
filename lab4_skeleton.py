c3ea5da27e227f3e3667af5e3c38c7eae3eabb42
Sofian Antri <santri@emi.u-bordeaux.fr>
2020-10-09 10:06:49 +0200

    previous year cnn

--- lab4_skeleton.py
+++ lab4_skeleton.py
@@ -73,3 +73,18 @@ y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes)
 #####TO COMPLETE
 
 
+def cnn():
+    model = Sequential()
+    model.add(Conv2D(32,
+                    kernel_size=(3,3),
+                    activation='relu',
+                    input_shape=(28, 28, 1)))
+    model.add(Conv2D(64,
+                    kernel_size=(3,3),
+                    activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
