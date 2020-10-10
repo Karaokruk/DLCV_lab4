5d79f654da7d786c480cf43017fafb29e9b17c1b
Sofian <sofian.antri@etu.u-bordeaux.fr>
2020-10-10 22:52:50 +0200

    one hot encode marche mais plot marche plus :c

--- lab4_2_skeleton.py
+++ lab4_2_skeleton.py
@@ -18,8 +18,8 @@ print('keras:', tensorflow.keras.__version__)
 
 ##Uncomment the following two lines if you get CUDNN_STATUS_INTERNAL_ERROR initialization errors.
 ## (it happens on RTX 2060 on room 104/moneo or room 204/lautrec)
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#physical_devices = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 #load (first download if necessary) the CIFAR10 dataset
@@ -35,14 +35,28 @@ print("a={}, b={}, c={}, d={}".format(a, b, c, d))
 ## Display one image and corresponding label
 import matplotlib
 import matplotlib.pyplot as plt
-i = np.random.randint(a)
-plt.imshow(x_train[i], cmap = matplotlib.cm.binary)
-plt.axis("off")
-plt.show()
+#i = np.random.randint(a)
+#plt.imshow(x_train[i], cmap = matplotlib.cm.binary)
+#plt.axis("off")
+#plt.show()
 #Let start our work: creating a convolutional neural network
 
 
-num_classes = 1
+num_classes = 10
+def one_hot_encode(y, digits):
+    print( "shape of y: ",y.shape)
+    examples = y.shape[0]
+    y = y.reshape(1, examples)
+    Y_new = np.eye(digits)[y.astype('int32')]  #shape (1, 70000, 10)
+    Y_new = Y_new.T.reshape(digits, examples)
+    print( "shape of encoded y: ",Y_new.shape)
+    Y_new = Y_new.T
+    return Y_new
+
+y_train = one_hot_encode(y_train, num_classes)
+y_test = one_hot_encode(y_test, num_classes)
+print(x_train.shape)
+print(y_train.shape)
 
 # NE PAS OUBLIER DE ONE HOT ENCODED XTRAIN ET YTARIN LOOOOL (peut-être aussi x_test & y_test)
 
@@ -57,12 +71,12 @@ def cnn():
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-nb_epochs = 10
+nb_epochs = 1
 batch_size = 64
 
 model = cnn()
 model.summary()
-callback = EarlyStopping(monitor='loss', patience=10)
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
 hist = model.fit(x_train,
                  y_train,
                  validation_data=(x_test, y_test),
@@ -70,10 +84,10 @@ hist = model.fit(x_train,
                  batch_size=batch_size,
                  callbacks=[callback])
 
-plt.plot(hist.history['accuracy'])
-plt.plot(hist.history['val_accuracy'])
-plt.title('model accuracy')
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.legend(['train','test'],loc='upper left')
-plt.show()
+#plt.plot(hist.history['accuracy'], list(range(hist.history['accuracy'])), label='accuracy')
+#plt.plot(hist.history['val_accuracy'])
+#plt.title('model accuracy')
+#plt.xlabel('epoch')
+#plt.ylabel('accuracy')
+#plt.legend(['train','test'],loc='upper left')
+#plt.show()
